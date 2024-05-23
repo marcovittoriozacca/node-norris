3d540bfa814b27f8e106546bb97d4cac684fb491
marcovittoriozacca <zaccamarco01@gmail.com>
2024-05-23 16:47:37 +0200

    feat: moved functions into utilities

--- server.js
+++ server.js
@@ -2,35 +2,12 @@ require('dotenv').config();
 const path = require('path');
 const fs = require('fs');
 const http = require('http');
-const {getJoke} = require('./utilities.js');
+const {getJoke,getFilePath,readJokesFile,readLastJoke,updateJokesFile} = require('./utilities.js');
 
 const port = process.env.PORT || 8000;
 const host = process.env.HOST || 'localhost';
 const apiUrl = process.env.API_URL || 'https://api.chucknorris.io/jokes/random';
 
-//get the absolute path of the given file with his extension
-const getFilePath = (fileName, extension) => {
-    const filePath = path.join(__dirname, fileName +'.'+ extension);
-    return filePath;
-}
-
-//reads the file by the given name - returns a json parsed file
-const readJokesFile = (fileName, extension) => {
-    const filePath = getFilePath(fileName, extension);
-    const file = fs.readFileSync(filePath, "utf-8");
-    return JSON.parse(file);
-}
-
-//returns the last joke
-const readLastJoke = ({value}) => value;
-
-
-//overwrite the given file
-const updateJokesFile = (fileName, extension, content) => {
-    const filePath = getFilePath(fileName, extension);
-    const string = JSON.stringify(content);
-    fs.writeFileSync(filePath, string);
-}
 
 const server = http.createServer((req,res) => {
 
@@ -39,9 +16,11 @@ const server = http.createServer((req,res) => {
     getJoke(apiUrl, (response) => {
 
         updateJokesFile('norrisDb','json', [...jokeFile, response.value]);
+
         res.writeHead(200, {"Content-Type": "text/html"});
-        res.end(`${readLastJoke(response)}`);
         
+        res.end(`${readLastJoke(response)}`);
+
     })
 });
 
--- utilities.js
+++ utilities.js
@@ -1,11 +1,50 @@
+const path = require('path');
+const fs = require('fs');
+
 const getJoke = (api, callback) => {
     fetch(api)
         .then(response => response.json())
         .then(data =>{
-            callback(data);
+            const file = readJokesFile('norrisDb', 'json');
+            if(file.includes(data.value)){
+                getJoke(api, callback);
+                return;
+            }else{
+                callback(data);
+            }
         });
 };
 
+//get the absolute path of the given file with his extension
+const getFilePath = (fileName, extension) => {
+    const filePath = path.join(__dirname, fileName +'.'+ extension);
+    return filePath;
+}
+
+//reads the file by the given name - returns a json parsed file
+const readJokesFile = (fileName, extension) => {
+    const filePath = getFilePath(fileName, extension);
+    const file = fs.readFileSync(filePath, "utf-8");
+    return JSON.parse(file);
+}
+
+//returns the last joke
+const readLastJoke = ({value}) => value;
+
+
+//overwrite the given file
+const updateJokesFile = (fileName, extension, content) => {
+    const filePath = getFilePath(fileName, extension);
+    const string = JSON.stringify(content);
+    fs.writeFileSync(filePath, string);
+}
+
+
+
 module.exports = {
     getJoke,
+    getFilePath,
+    readJokesFile,
+    readLastJoke,
+    updateJokesFile
 }
